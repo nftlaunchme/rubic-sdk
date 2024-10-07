afe8550c0ad5bd1c3c822b984ef29890fd631e6f
nftlaunchme <usexportinternational11@gmail.com>
2024-10-07 18:34:53 +0100

    Update vvs-finance-provider.ts

--- src/features/on-chain/calculation-manager/providers/dexes/cronos/vvs-finance/vvs-finance-provider.ts
+++ src/features/on-chain/calculation-manager/providers/dexes/cronos/vvs-finance/vvs-finance-provider.ts
@@ -1,10 +1,11 @@
-import { OnChainTradeType } from "../../../../core/blockchain/models/on-chain-trade-type";
 import { UniswapV2AbstractProvider } from "../common/uniswap-v2-abstract/uniswap-v2-abstract-provider";
+import { BLOCKCHAIN_NAME } from "../../../../core/blockchain/models/blockchain-name";
 
 export class VvsFinanceProvider extends UniswapV2AbstractProvider {
-    public readonly type = OnChainTradeType.VVS_FINANCE;
-
+    public readonly blockchain = BLOCKCHAIN_NAME.CRONOS;
+    
     public readonly providerSettings = {
+        allowedSlippages: [0.01, 0.03],
         routingProviderId: 'vvs-finance',
         liquidityProviderName: 'VVS Finance'
     };
