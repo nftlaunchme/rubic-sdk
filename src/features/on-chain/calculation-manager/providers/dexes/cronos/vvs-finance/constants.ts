8ef803637aa365246a0dd66fb7b4922171136a2e
nftlaunchme <usexportinternational11@gmail.com>
2024-10-07 18:22:00 +0100

    Create constants.ts

---
+++ src/features/on-chain/calculation-manager/providers/dexes/cronos/vvs-finance/constants.ts
@@ -0,0 +1,8 @@
+import { UniswapV2ProviderConfiguration } from "../common/uniswap-v2-abstract/models/uniswap-v2-provider-configuration";
+
+export const VVS_FINANCE_PROVIDER_CONFIGURATION: UniswapV2ProviderConfiguration = {
+    wethAddress: '0x5C7F8A570d578ED84E63fdFA7b1eE72dEae1AE23',
+    routerAddress: '0x145863Eb42Cf62847A6Ca784e6416C1682b1b2Ae',
+    factoryAddress: '0x3B44B2a187a7b3824131F8db5a74194D0a42Fc15',
+    maxTransitTokens: 2
+};
